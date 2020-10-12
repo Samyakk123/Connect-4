18afd8feca7d67055b9e44166482ac42857e87ac
AryPat <69769265+AryPat@users.noreply.github.com>
2020-10-12 17:00:12 -0400

    Visuals

--- Connect4/src/Difficulty.java
+++ Connect4/src/Difficulty.java
@@ -13,6 +13,8 @@ public class Difficulty extends JFrame {
 
   private JPanel contentPane;
   private JFrame current;
+  private JLabel easyDifficulty, easyDifficultySmall;
+ 
 
   /**
    * Create the frame.
@@ -20,13 +22,14 @@ public class Difficulty extends JFrame {
   public Difficulty() {
     this.current = this;
     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    setBounds(100, 100, 800, 550);
+    setBounds(100, 100, 800, 551);
     contentPane = new JPanel();
     contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
     setContentPane(contentPane);
     contentPane.setLayout(null);
     
-    JLabel veryHardDifficulty = new JLabel("Very hard");
+    JLabel veryHardDifficulty = new JLabel("");
+    veryHardDifficulty.setIcon(new ImageIcon(Difficulty.class.getResource("/images/VeryHardBig.png")));
     veryHardDifficulty.addMouseListener(new MouseAdapter() {
       @Override
       public void mousePressed(MouseEvent e) {
@@ -37,52 +40,10 @@ public class Difficulty extends JFrame {
         
       }
     });
-    veryHardDifficulty.setBounds(422, 340, 185, 41);
-    contentPane.add(veryHardDifficulty);
     
-    JLabel hardDifficulty = new JLabel("Hard");
-    hardDifficulty.addMouseListener(new MouseAdapter() {
-      @Override
-      public void mousePressed(MouseEvent e) {
-        Play frame = new Play(false, 5);
-        frame.setVisible(true);
-        current.setVisible(false);
-        current.dispose();
-        
-      }
-    });
-    hardDifficulty.setBounds(119, 336, 185, 48);
-    contentPane.add(hardDifficulty);
-    
-    JLabel mediumDifficulty = new JLabel("Medium");
-    mediumDifficulty.addMouseListener(new MouseAdapter() {
-      @Override
-      public void mousePressed(MouseEvent e) {
-        Play frame = new Play(false, 4);
-        frame.setVisible(true);
-        current.setVisible(false);
-        current.dispose();
-        
-      }
-    });
-    mediumDifficulty.setBounds(537, 221, 185, 48);
-    contentPane.add(mediumDifficulty);
-    
-    JLabel easyDifficulty = new JLabel("Easy");
-    easyDifficulty.addMouseListener(new MouseAdapter() {
-      @Override
-      public void mousePressed(MouseEvent e) {
-        Play frame = new Play(false, 3);
-        frame.setVisible(true);
-        current.setVisible(false);
-        current.dispose();
-        
-      }
-    });
-    easyDifficulty.setBounds(263, 221, 185, 48);
-    contentPane.add(easyDifficulty);
-    
-    JLabel veryEasyDifficulty = new JLabel("Very easy");
+    JLabel veryEasyDifficulty = new JLabel("");
+    veryEasyDifficulty.setIcon(new ImageIcon(Difficulty.class.getResource("/images/VeryEasyBig.png")));
+    veryEasyDifficulty.setVisible(true);
     veryEasyDifficulty.addMouseListener(new MouseAdapter() {
       @Override
       public void mousePressed(MouseEvent e) {
@@ -93,17 +54,112 @@ public class Difficulty extends JFrame {
         
       }
     });
-    veryEasyDifficulty.setBounds(36, 210, 149, 48);
-    contentPane.add(veryEasyDifficulty);
     
-    JLabel title = new JLabel("Choose Your Difficulty!");
-    title.setBounds(273, 68, 342, 61);
+    JLabel easyDifficultySmall = new JLabel("");
+    easyDifficultySmall.addMouseListener(new MouseAdapter() {
+        @Override
+        public void mouseEntered(MouseEvent e) {
+            easyDifficulty.setVisible(true);
+            easyDifficultySmall.setVisible(false);
+        }
+        public void mouseExited(MouseEvent e) {
+            easyDifficulty.setVisible(false);
+            easyDifficultySmall.setVisible(true);
+        }
+    });
+    easyDifficultySmall.setIcon(new ImageIcon(Difficulty.class.getResource("/images/EasySmall.png")));
+    easyDifficultySmall.setBounds(105, 186, 112, 92);
+    contentPane.add(easyDifficultySmall);
+    veryEasyDifficulty.setBounds(201, 322, 137, 111);
+    contentPane.add(veryEasyDifficulty);
+    veryHardDifficulty.setBounds(412, 322, 158, 111);
+    contentPane.add(veryHardDifficulty);
+    easyDifficultySmall.setVisible(true);
+    
+    JLabel hardDifficulty = new JLabel("");
+    hardDifficulty.setIcon(new ImageIcon(Difficulty.class.getResource("/images/HardBig.png")));
+    hardDifficulty.addMouseListener(new MouseAdapter() {
+      @Override
+      public void mousePressed(MouseEvent e) {
+        Play frame = new Play(false, 5);
+        frame.setVisible(true);
+        current.setVisible(false);
+        current.dispose();
+        
+      }
+    });
+    hardDifficulty.setBounds(528, 207, 158, 61);
+    contentPane.add(hardDifficulty);
+    
+    JLabel mediumDifficulty = new JLabel("");
+    mediumDifficulty.setIcon(new ImageIcon(Difficulty.class.getResource("/images/MediumBig.png")));
+    mediumDifficulty.addMouseListener(new MouseAdapter() {
+      @Override
+      public void mousePressed(MouseEvent e) {
+        Play frame = new Play(false, 4);
+        frame.setVisible(true);
+        current.setVisible(false);
+        current.dispose();
+        
+      }
+    });
+    mediumDifficulty.setBounds(278, 207, 218, 61);
+    contentPane.add(mediumDifficulty);
+    
+    easyDifficulty = new JLabel("");
+    easyDifficulty.setIcon(new ImageIcon(Difficulty.class.getResource("/images/EasyBig.png")));
+    easyDifficulty.setVisible(false);
+    easyDifficulty.addMouseListener(new MouseAdapter() {
+      @Override
+      public void mousePressed(MouseEvent e) {
+        Play frame = new Play(false, 3);
+        frame.setVisible(true);
+        current.setVisible(false);
+        current.dispose();
+        
+      }
+      public void mouseEntered(MouseEvent e) {
+          easyDifficulty.setVisible(true);
+          easyDifficultySmall.setVisible(false);
+      }
+      public void mouseExited(MouseEvent e) {
+          easyDifficulty.setVisible(false);
+          easyDifficultySmall.setVisible(true);
+      }
+    });
+    easyDifficulty.setBounds(95, 207, 137, 61);
+    contentPane.add(easyDifficulty);
+    
+    JLabel title = new JLabel("");
+    title.setVisible(true);
+    title.setIcon(new ImageIcon(Difficulty.class.getResource("/images/ChooseDifficulty.png")));
+    title.setBounds(52, 47, 692, 61);
     contentPane.add(title);
     
     JLabel Background = new JLabel("");
     Background.setIcon(new ImageIcon(Play.class.getResource("/images/BlueBackgronud.jpg")));
     Background.setBounds(0, 0, 784, 512);
     contentPane.add(Background);
+    
+    JLabel mediumDfficultySmall = new JLabel("");
+    mediumDfficultySmall.setIcon(new ImageIcon(Difficulty.class.getResource("/images/MediumSmall.png")));
+    mediumDfficultySmall.setBounds(346, 224, 61, 16);
+    contentPane.add(mediumDfficultySmall);
+    
+    JLabel hardDifficultySmall = new JLabel("");
+    hardDifficultySmall.setIcon(new ImageIcon(Difficulty.class.getResource("/images/HardSmall.png")));
+    hardDifficultySmall.setBounds(564, 224, 61, 16);
+    contentPane.add(hardDifficultySmall);
+    
+    JLabel veryEasyDifficultySmall = new JLabel("");
+    veryEasyDifficultySmall.setIcon(new ImageIcon(Difficulty.class.getResource("/images/VeryEasySmall.png")));
+    veryEasyDifficultySmall.setBounds(235, 364, 61, 16);
+    contentPane.add(veryEasyDifficultySmall);
+    
+    JLabel veryHardDifficultySmall = new JLabel("");
+    veryHardDifficultySmall.setIcon(new ImageIcon(Difficulty.class.getResource("/images/VeryHardSmall.png")));
+    veryHardDifficultySmall.setBounds(448, 364, 61, 16);
+    contentPane.add(veryHardDifficultySmall);
     
     
     
